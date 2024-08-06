f77b4a3989417f022937eab995339cee9d57b22a
Ethan Niser <ethan@niser.com>
2024-08-06 04:43:44 -0500

    add comment

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -63,6 +63,9 @@ local servers = {
 
 local function setup_server(server_name)
   local server = servers[server_name] or {}
+  -- This handles overriding only values explicitly passed
+  -- by the server configuration above. Useful when disabling
+  -- certain features of an LSP (for example, turning off formatting for tsserver)
   server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
   lspconfig[server_name].setup(server)
 end
