d52097d54cf598b94e099834af2f82af4f19dd02
Ethan Niser <ethan@niser.com>
2024-08-06 07:21:07 -0500

    2024-08-06 07:21:07

---
+++ nvim/plugin/lazygit.lua
@@ -0,0 +1,9 @@
+vim.keymap.set('n', 'gg', '<cmd>Lazygit<cr>', { desc = 'Open parent directory' })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  desc = 'Set lazygit project root when opening a new buffer',
+  group = vim.api.nvim_create_augroup('lazygit-set-root', { clear = true }),
+  callback = function()
+    require('lazygit.utils').project_root_dir()
+  end,
+})
