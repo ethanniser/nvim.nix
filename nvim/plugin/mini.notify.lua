7dfbbe81aa6338e810673f639cd2d7a687c9e882
Ethan Niser <ethan@niser.com>
2024-08-06 16:44:18 -0500

    adjust lsp notify window config

--- nvim/plugin/mini.notify.lua
+++ nvim/plugin/mini.notify.lua
@@ -2,17 +2,10 @@ require('mini.notify').setup {
   window = {
     -- Move window to bottom right
     config = function()
-      local cols = vim.o.columns
       local lines = vim.o.lines
       return {
-        width = math.floor(cols * 0.382),
-        height = 3, -- You can adjust this value as needed
         anchor = 'SE',
-        col = cols - 1,
         row = lines - 2,
-        border = 'single',
-        zindex = 999,
-        style = 'minimal',
       }
     end,
   },
