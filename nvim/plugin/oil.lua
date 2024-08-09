3fb4808d14f34e37fee65e4c363358fb14ae7351
Ethan Niser <whatplan00@gmail.com>
2024-08-09 00:59:32 -0500

    adjust oil config

--- nvim/plugin/oil.lua
+++ nvim/plugin/oil.lua
@@ -1,3 +1,19 @@
-require('oil').setup {}
+require('oil').setup {
+  delete_to_trash = true,
+  skip_confirm_for_simple_edits = true,
+  view_options = {
+    show_hidden = true,
+    natural_order = true,
+    is_always_hidden = function(name, _)
+      return name == '..' or name == '.git'
+    end,
+  },
+  win_options = {
+    wrap = true,
+  },
+  float = {
+    padding = 5,
+  },
+}
 
-vim.keymap.set('n', '-', '<cmd>Oil<cr>', { desc = 'Open parent directory' })
+vim.keymap.set('n', '-', '<cmd>Oil --float<cr>', { desc = 'Open parent directory' })
