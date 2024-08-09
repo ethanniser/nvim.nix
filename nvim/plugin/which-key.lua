d436f726da2a886d2cd36f594c91e57c9b077ae9
Ethan Niser <whatplan00@gmail.com>
2024-08-09 00:59:37 -0500

    adjust which key config

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -1,9 +1,9 @@
 local wk = require('which-key')
 wk.add {
-    { '<leader>c', group = '[C]ode' },
-    { '<leader>d', group = '[D]ocument' },
-    { '<leader>r', group = '[R]ename' },
-    { '<leader>s', group = '[S]earch' },
-    { '<leader>w', group = '[W]orkspace' },
-    { '<leader>h', group = '[H]arpoon' },
-  }
+  { '<leader>c', group = '[C]ode' },
+  { '<leader>d', group = '[D]ocument' },
+  { '<leader>r', group = '[R]ename' },
+  { '<leader>s', group = '[S]earch' },
+  { '<leader>w', group = '[W]orkspace' },
+  { '<leader>h', group = '[H]arpoon' },
+}
