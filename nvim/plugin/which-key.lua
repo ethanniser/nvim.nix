ac2d01243eaae482a5c44716b69804a52b3eab04
Ethan Niser <ethan@niser.com>
2024-08-09 10:13:17 -0500

    2024-08-09 10:13:17

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -1,9 +1,11 @@
-local wk = require('which-key')
-wk.add {
-  { '<leader>c', group = '[C]ode' },
-  { '<leader>d', group = '[D]ocument' },
-  { '<leader>r', group = '[R]ename' },
-  { '<leader>s', group = '[S]earch' },
-  { '<leader>w', group = '[W]orkspace' },
-  { '<leader>h', group = '[H]arpoon' },
+require('which-key').setup {
+  spec = {
+    { '<leader>c', group = '[C]ode' },
+    { '<leader>d', group = '[D]ocument' },
+    { '<leader>r', group = '[R]ename' },
+    { '<leader>s', group = '[S]earch' },
+    { '<leader>w', group = '[W]orkspace' },
+    { '<leader>h', group = '[H]arpoon' },
+    { '<leader>g', group = '[G]it' },
+  },
 }
