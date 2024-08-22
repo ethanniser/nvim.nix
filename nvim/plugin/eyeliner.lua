22d5aef30665ee9936eb107cfb09dd877a13f2a7
Ethan Niser <ethan@niser.com>
2024-08-22 09:31:24 -0400

    idk what fixed it but yea

--- nvim/plugin/eyeliner.lua
+++ nvim/plugin/eyeliner.lua
@@ -1,3 +1,3 @@
-require('eyeliner').setup {
-  highlight_on_key = true, -- this must be set to true for dimming to work!
-}
+-- require('eyeliner').setup {
+--   highlight_on_key = true, -- this must be set to true for dimming to work!
+-- }
