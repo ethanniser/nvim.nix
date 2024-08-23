d45dc4f49865656693fc41d33d2921625dc7e09c
Ethan Niser <ethan@niser.com>
2024-08-23 12:22:18 -0400

    python lsp look for venv first

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -2,6 +2,28 @@ local lspconfig = require('lspconfig')
 
 vim.diagnostic.config { update_in_insert = false }
 
+local util = require('lspconfig/util')
+
+local path = util.path
+
+local function get_python_path(workspace)
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+  end
+
+  -- Find and use virtualenv in workspace directory.
+  for _, pattern in ipairs { '*', '.*' } do
+    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+    if match ~= '' then
+      return path.join(path.dirname(match), 'bin', 'python')
+    end
+  end
+
+  -- Fallback to system Python.
+  return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
+end
+
 -- LSP servers and clients are able to communicate to each other what features they support.
 --  By default, Neovim doesn't support everything that is in the LSP Specification.
 --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
@@ -64,7 +86,11 @@ local servers = {
   astro = {},
 
   -- Python
-  pyright = {},
+  pyright = {
+    before_init = function(_, config)
+      config.settings.python.pythonPath = get_python_path(config.root_dir)
+    end,
+  },
 }
 
 local function setup_server(server_name)
