7b6591ab88f7483bb459dc541a55713b5ad87c3a
Ethan Niser <ethan@niser.com>
2024-10-15 01:26:28 -0400

    add autocomannd to enter terminal mode in insert by default

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -27,3 +27,11 @@ vim.api.nvim_create_user_command('FormatEnable', function(args)
   vim.b.disable_autoformat = false
   vim.g.disable_autoformat = false
 end, { desc = 'Re-enable autoformat-on-save' })
+
+-- Automatically enter insert mode when opening a terminal
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = '*',
+  callback = function()
+    vim.cmd('startinsert')
+  end,
+})
