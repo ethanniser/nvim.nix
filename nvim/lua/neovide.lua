5614a6e4daad07e99a38a8532d205045c4dd8c3a
Ethan Niser <ethan@niser.com>
2025-04-24 21:39:21 -0400

    update keymaps

--- nvim/lua/neovide.lua
+++ nvim/lua/neovide.lua
@@ -4,18 +4,18 @@ vim.g.neovide_cursor_trail_size = 0.4
 vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
 
 vim.o.guifont = 'MesloLGS Nerd Font:h14'
-vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
-vim.keymap.set('v', '<D-c>', '"+y') -- Copy
-vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
-vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
-vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
-vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+-- General clipboard mappings using Command key
+vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save file in normal mode
+vim.keymap.set('v', '<D-c>', '"+y') -- Copy selection to system clipboard in visual mode
 
--- Allow clipboard copy paste in neovim
-vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+-- Paste from system clipboard using Command key across different modes
+vim.keymap.set('n', '<D-v>', '"+P') -- Paste after cursor in normal mode
+vim.keymap.set('v', '<D-v>', '"+P') -- Paste after selection in visual mode
+vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste into command-line
+vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste into insert mode, maintaining position
+vim.keymap.set('', '<D-v>', '+p<CR>', { noremap = true, silent = true }) -- Paste in modes not explicitly mapped
+vim.keymap.set('!', '<D-v>', '<C-R>+', { noremap = true, silent = true }) -- Paste in command-line mode (alternative/redundant but kept)
+vim.keymap.set('t', '<D-v>', '<C-R>+', { noremap = true, silent = true }) -- Paste into terminal mode
 
 -- Dynamically change scale
 vim.g.neovide_scale_factor = 1.0
@@ -23,29 +23,8 @@ local change_scale_factor = function(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
 vim.keymap.set('n', '<C-=>', function()
-  change_scale_factor(1.25)
+  change_scale_factor(1.10)
 end)
 vim.keymap.set('n', '<C-->', function()
-  change_scale_factor(1 / 1.25)
-end)
-
-vim.g.transparency = 0.8
-
-local change_transparency = function(delta)
-  local next_value = vim.g.transparency + delta
-  if next_value > 1.0 or next_value < 0.0 then
-    return
-  else
-    vim.g.transparency = next_value
-  end
-end
-vim.keymap.set({ 'n', 'v', 'o' }, '<D-]>', function()
-  change_transparency(0.05)
-end)
-vim.keymap.set({ 'n', 'v', 'o' }, '<D-[>', function()
-  change_transparency(-0.05)
-end)
-
-vim.keymap.set({ 'n', 'v', 'o' }, '<D-;>', function()
-  vim.g.neovide_window_blurred = not vim.g.neovide_window_blurred
+  change_scale_factor(1 / 1.10)
 end)
